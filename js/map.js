c3b0f4243c51f52349e861714bfd45d6eb0a81a3
1k-bhdn <1k.bhdn@gmail.com>
2020-11-10 18:58:39 +0200

    Завершил 6.1

--- js/map.js
+++ js/map.js
@@ -7,15 +7,30 @@
   const FILTERS_FORM_SELECT = FILTERS_FORM.querySelectorAll(`select`);
   const FILTERS_FORM_FIELDSET = FILTERS_FORM.querySelector(`#housing-features`);
   const PINS_AREA = document.querySelector(`.map__pins`);
+  const adCount = 8;
   const AD_PINS_CONTAINER = document.createDocumentFragment();
   const AD_CARDS_CONTAINER = document.createDocumentFragment();
-  const adCount = window.data.adCount;
-  const offersData = window.data.offersData;
 
-  for (let i = 0; i <= adCount - 1; i++) {
-    AD_PINS_CONTAINER.appendChild(window.pin.renderAdPin(offersData[i]));
-    AD_CARDS_CONTAINER.appendChild(window.card.renderAdCard(offersData[i]));
-  }
+  const successHandler = (offersData) => {
+    for (let i = 0; i <= adCount - 1; i++) {
+      AD_PINS_CONTAINER.appendChild(window.pin.renderAdPin(offersData[i]));
+      AD_CARDS_CONTAINER.appendChild(window.card.renderAdCard(offersData[i]));
+    }
+  };
+
+  const errorHandler = (errorMessage) => {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red; color: white; padding: 10px;`;
+    node.style.position = `absolute`;
+    node.style.left = `0`;
+    node.style.right = `0`;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  window.server.download(successHandler, errorHandler);
 
   window.map = {
     toggleFieldsAvailability: (display) => {
